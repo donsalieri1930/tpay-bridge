c18731a11ded31deb120bc221a722f48be78b370
don <don>
2026-03-17 12:10:15 +0100

    update frontend url

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ CHARGEBACK_UPDATES_STATUS = True
 HTTPX_REQUEST_TIMEOUT = 5.0
 TPAY_TOKEN_URL = 'https://api.tpay.com'
 TPAY_CERT_URL = 'https://secure.tpay.com/x509/notifications-jws.pem'
-FRONTEND_URL = 'https://pay.wegielek.edu.pl'
+FRONTEND_URL = 'https://pay.zar.edu.pl'
 
 if SANDBOX:
     TPAY_CERT_URL = 'https://secure.sandbox.tpay.com/x509/notifications-jws.pem'
